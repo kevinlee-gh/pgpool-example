8cf02941dae11702c4b3fa3d318772bdf77d423e
kienlt <kienlt@topcv.vn>
2025-08-20 16:54:15 +0700

    feat: use sqlalchemy

--- client/main.py
+++ client/main.py
@@ -1,32 +1,40 @@
 import os
 import time
+import logging
 
+from sqlalchemy import create_engine, inspect
+from sqlalchemy.orm import Session
 import psycopg
 from locust import User, TaskSet, task, between, events
 
+from database.schema.stress_test.counter import Counter
+
+logging.basicConfig(
+    level = logging.INFO,
+    format="%(levelname)s - %(message)s"
+)
+
+logger = logging.getLogger(__name__)
 
 class PostgresClient:
-    def __init__(self, conn_string):
-        self.db_conn = psycopg.connect(conn_string)
-
-    def execute_query(self, query):
-        with self.db_conn.cursor() as cursor:
-            cursor.execute(query)
-            return cursor.fetchall()
+    def __init__(self):
+        engine = create_engine(
+            f"postgresql+psycopg2://postgres:postgres123@localhost:5432/postgres",
+            pool_timeout=60
+        )
+        self.db_session = Session(engine)
 
 # This class will be executed when you run locust
 class PostgresLocust(User):
     min_wait = 0
     max_wait = 1
     wait_time = between(min_wait, max_wait)
-    conn_string = "postgresql://postgres:postgres123@localhost:5432/postgres"
 
     def __init__(self, *args):
         super().__init__(*args)
-        self.client = PostgresClient(self.conn_string)
+        self.client = PostgresClient()
 
     @task
     def run_query(self):
-        self.client.execute_query(
-            f"SELECT * FROM stress_test.counter;",
-        )
+        res = self.client.db_session.query(Counter).all()
+        logger.info(f"Query executed successfully - {len(res)}")
