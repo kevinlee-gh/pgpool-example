d950c54e486c2ff63f39060e8af1fdee95d5ff39
kienlt <kienlt@topcv.vn>
2025-09-04 11:35:11 +0700

    feat(client): init components

--- client/components/client.py
+++ client/components/client.py
@@ -3,6 +3,7 @@ from sqlalchemy.schema import CreateSchema
 from sqlalchemy.orm import Session
 
 from database.schema import LS_SCHEMA
+from database.schema.stress_test.counter import Counter
 from components.logger import logger
 
 class PostgresClient:
@@ -16,7 +17,7 @@ class PostgresClient:
         self.engine = create_engine(f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}")
         self.db_session = Session(self.engine)
 
-    def setup_database(self):
+    def setup_database(self, n= 20):
         for schema in LS_SCHEMA:
             with self.engine.connect() as connection:
                 if not inspect(connection).has_schema(schema.__table_args__['schema']):
@@ -27,4 +28,14 @@ class PostgresClient:
                     logger.info(f"Schema '{schema.__table_args__['schema']}' already exists.")
 
             schema.metadata.create_all(self.engine)
-        return 
+
+        for i in range(20):
+            if self.db_session.query(Counter).filter(Counter.id == i).all():
+                continue
+            counter = Counter(
+                id = i,
+                value= 0, count=0
+            )
+            self.db_session.add(counter)
+            self.db_session.commit()
+        return
