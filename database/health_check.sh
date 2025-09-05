81537cf2aa07cfcaf853170eb9e5665ddf3f2af5
kienlt <kienlt@topcv.vn>
2025-09-05 17:00:24 +0700

    feat: nth

--- database/health_check.sh
+++ database/health_check.sh
@@ -17,4 +17,4 @@ sync_count=$(psql -tf /ha-scripts/sync_standby_count.sql postgres)
 
 current_sync_count=$(psql -Atc "SELECT COUNT(*) FROM pg_stat_replication WHERE sync_state = 'sync'" postgres)
 
-echo $sync_count - $current_sync_count
+echo $sync_count - $current_sync_count - $(if [ $sync_count == $current_sync_count ]; then echo "OK"; else echo "NOT OK"; exit 1; fi)
