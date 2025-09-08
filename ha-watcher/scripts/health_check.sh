1214c8d6d4d7c85a4d251181f9cee1873681e120
kienlt <kienlt@topcv.vn>
2025-09-08 10:59:24 +0700

    feat: add date to ha-watcher

--- ha-watcher/scripts/health_check.sh
+++ ha-watcher/scripts/health_check.sh
@@ -9,4 +9,9 @@ sync_count=$(psql -tf /sql/sync_standby_count.sql postgres)
 
 current_sync_count=$(psql -Atc "SELECT COUNT(*) FROM pg_stat_replication WHERE sync_state = 'sync'" postgres)
 
-echo $sync_count - $current_sync_count - $(if [ $sync_count == $current_sync_count ]; then echo "OK"; else echo "NOT OK"; exit 1; fi)
+if [ $sync_count == $current_sync_count ]; then
+    echo $(date +%Y-%m-%dT%H:%M:%S) - Synchronous standby check: OK
+else
+    echo $(date +%Y-%m-%dT%H:%M:%S) - Synchronous standby check: NOT OK
+    exit 1
+fi
